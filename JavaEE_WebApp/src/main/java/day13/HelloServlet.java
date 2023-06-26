fe1d66c591b50246b67ea45d05a06df89c4c76af
Tejas S Manjrekar <tejasman1999@gmail.com>
2023-06-26 16:20:40 +0530

    JAva Web APP Project

---
+++ JavaEE_WebApp/src/main/java/day13/HelloServlet.java
@@ -0,0 +1,31 @@
+package day13;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String MIMEType ="text/html";
+		response.setContentType(MIMEType);
+		PrintWriter out=response.getWriter();
+		
+		String responseText="<h1 style='color:red'>Hello World from Servlet</h1>";
+		out.println(responseText);
+	}
+
+}
